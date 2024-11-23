7e1a2831321564056cd983c1a2e6069e81be81c5
gonpra <gonpra@pm.me>
2024-11-23 19:56:59 -0300

    add binaries

---
+++ binaries/linux/CMakeFiles/PIM.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PIM.dir/src/callbacks.c.o"
+  "CMakeFiles/PIM.dir/src/callbacks.c.o.d"
+  "CMakeFiles/PIM.dir/src/components/table-company.c.o"
+  "CMakeFiles/PIM.dir/src/components/table-company.c.o.d"
+  "CMakeFiles/PIM.dir/src/db.c.o"
+  "CMakeFiles/PIM.dir/src/db.c.o.d"
+  "CMakeFiles/PIM.dir/src/main.c.o"
+  "CMakeFiles/PIM.dir/src/main.c.o.d"
+  "CMakeFiles/PIM.dir/src/structs.c.o"
+  "CMakeFiles/PIM.dir/src/structs.c.o.d"
+  "CMakeFiles/PIM.dir/src/ui.c.o"
+  "CMakeFiles/PIM.dir/src/ui.c.o.d"
+  "CMakeFiles/PIM.dir/src/usecase.c.o"
+  "CMakeFiles/PIM.dir/src/usecase.c.o.d"
+  "CMakeFiles/PIM.dir/src/util.c.o"
+  "CMakeFiles/PIM.dir/src/util.c.o.d"
+  "PIM"
+  "PIM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PIM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ binaries/linux/CMakeFiles/PIM.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PIM.
+# This may be replaced when dependencies are built.
