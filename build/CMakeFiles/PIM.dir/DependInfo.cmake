701d348e3100c5c08f4e85c5caaee2a2418a21f4
gonpra <gonpra@pm.me>
2024-11-23 20:01:16 -0800

    add local libraries

---
+++ build/CMakeFiles/PIM.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Docker/Downloads/pimiv/src/callbacks.c" "CMakeFiles/PIM.dir/src/callbacks.c.obj" "gcc" "CMakeFiles/PIM.dir/src/callbacks.c.obj.d"
+  "C:/Users/Docker/Downloads/pimiv/src/components/table-company.c" "CMakeFiles/PIM.dir/src/components/table-company.c.obj" "gcc" "CMakeFiles/PIM.dir/src/components/table-company.c.obj.d"
+  "C:/Users/Docker/Downloads/pimiv/src/db.c" "CMakeFiles/PIM.dir/src/db.c.obj" "gcc" "CMakeFiles/PIM.dir/src/db.c.obj.d"
+  "C:/Users/Docker/Downloads/pimiv/src/main.c" "CMakeFiles/PIM.dir/src/main.c.obj" "gcc" "CMakeFiles/PIM.dir/src/main.c.obj.d"
+  "C:/Users/Docker/Downloads/pimiv/src/structs.c" "CMakeFiles/PIM.dir/src/structs.c.obj" "gcc" "CMakeFiles/PIM.dir/src/structs.c.obj.d"
+  "C:/Users/Docker/Downloads/pimiv/src/ui.c" "CMakeFiles/PIM.dir/src/ui.c.obj" "gcc" "CMakeFiles/PIM.dir/src/ui.c.obj.d"
+  "C:/Users/Docker/Downloads/pimiv/src/usecase.c" "CMakeFiles/PIM.dir/src/usecase.c.obj" "gcc" "CMakeFiles/PIM.dir/src/usecase.c.obj.d"
+  "C:/Users/Docker/Downloads/pimiv/src/util.c" "CMakeFiles/PIM.dir/src/util.c.obj" "gcc" "CMakeFiles/PIM.dir/src/util.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PIM.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PIM.dir/src/callbacks.c.obj"
+  "CMakeFiles/PIM.dir/src/callbacks.c.obj.d"
+  "CMakeFiles/PIM.dir/src/components/table-company.c.obj"
+  "CMakeFiles/PIM.dir/src/components/table-company.c.obj.d"
+  "CMakeFiles/PIM.dir/src/db.c.obj"
+  "CMakeFiles/PIM.dir/src/db.c.obj.d"
+  "CMakeFiles/PIM.dir/src/main.c.obj"
+  "CMakeFiles/PIM.dir/src/main.c.obj.d"
+  "CMakeFiles/PIM.dir/src/structs.c.obj"
+  "CMakeFiles/PIM.dir/src/structs.c.obj.d"
+  "CMakeFiles/PIM.dir/src/ui.c.obj"
+  "CMakeFiles/PIM.dir/src/ui.c.obj.d"
+  "CMakeFiles/PIM.dir/src/usecase.c.obj"
+  "CMakeFiles/PIM.dir/src/usecase.c.obj.d"
+  "CMakeFiles/PIM.dir/src/util.c.obj"
+  "CMakeFiles/PIM.dir/src/util.c.obj.d"
+  "PIM.exe"
+  "PIM.exe.manifest"
+  "PIM.pdb"
+  "libPIM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PIM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
